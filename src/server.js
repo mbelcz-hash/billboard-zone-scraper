f1f9e2271c3c3fc5de55a3502ff064bd7768afbf
mbelcz-hash <mbelcz@gmail.com>
2026-07-22 15:33:12 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { runFullCorridorScan } = require('../zone-scraper-system');
+const { runFullCorridorScan } = require('./zone-scraper-system');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
